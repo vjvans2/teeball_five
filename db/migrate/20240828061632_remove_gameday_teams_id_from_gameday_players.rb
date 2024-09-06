078526cce53de6fa48cc2182bb672e2cd9789b3a
turonn <72614966+turonn@users.noreply.github.com>
2024-09-06 19:26:12 -0400

    adjusts migration to skip if the column has already been removed as I think is the case

--- db/migrate/20240828061632_remove_gameday_teams_id_from_gameday_players.rb
+++ db/migrate/20240828061632_remove_gameday_teams_id_from_gameday_players.rb
@@ -1,5 +1,9 @@
 class RemoveGamedayTeamsIdFromGamedayPlayers < ActiveRecord::Migration[7.2]
   def change
-    remove_column :gameday_players, :gameday_teams_id, :bigint
+    if column_exists?(:gameday_players, :gameday_teams_id)
+      remove_column :gameday_players, :gameday_teams_id
+    else
+      puts "Column gameday_team_id does not exist in gameday_players"
+    end
   end
 end
