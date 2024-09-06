2f1adc7be55e5ca58bcaea432e5eaa37628f2a12
turonn <72614966+turonn@users.noreply.github.com>
2024-09-06 19:53:48 -0400

    adds schema annotations automatically generated by the annotaterb gem

--- app/models/inning.rb
+++ app/models/inning.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: innings
+#
+#  id            :bigint           not null, primary key
+#  inning_number :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  game_id       :bigint           not null
+#
+# Indexes
+#
+#  index_innings_on_game_id  (game_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (game_id => games.id)
+#
 class Inning < ApplicationRecord
   has_one :game
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: teams
+#
+#  id               :bigint           not null, primary key
+#  city             :string
+#  name             :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  gameday_teams_id :bigint
+#
+# Indexes
+#
+#  index_teams_on_gameday_teams_id  (gameday_teams_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (gameday_teams_id => gameday_teams.id)
+#
 class Team < ApplicationRecord
   has_many :players
   has_many :coaches
