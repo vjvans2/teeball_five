3c5cb51270a8e4ecffe0d648c33b876d7fab53c2
turonn <72614966+turonn@users.noreply.github.com>
2024-09-07 17:29:24 -0400

    Factory Changes to support new relationship

--- spec/factories/seasons.rb
+++ spec/factories/seasons.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :season do
     team { build(:team) }
-    # games { build(:game) } Is this a typo? Should each game belong to a season? Or can a game be a part of different seasons?
   end
 end
