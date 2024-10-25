12204ece750501aced06d3536a8965e9b1f4267c
Vinny VanSlyke <vincent.vanslyke@gmail.com>
2024-10-24 21:01:34 -0400

    cleaned up all other existing tests

--- app/controllers/gameday_players_controller.rb
+++ app/controllers/gameday_players_controller.rb
@@ -1,7 +1,9 @@
 class GamedayPlayersController < ApplicationController
   def show
+    render status: :ok
   end
 
   def post
+    render status: :ok
   end
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -2,29 +2,37 @@ require 'rails_helper'
 
 RSpec.describe Player, type: :model do
   describe 'validations' do
-    let(:player) { build(:player) }
+    let(:team) { create(:team) }
+    let(:player) { create(:player, team: team) }
 
     it "has a working factory" do
       expect(player).to be_valid
     end
 
     context "without a first name" do
-      let(:player) { build(:player, first_name: nil) }
+      let(:team) { create(:team) }
+      let(:player) { create(:player, team: team, first_name: nil) }
 
-      it { expect(player).not_to be_valid }
+      it 'throws an error' do
+        expect { player }.to raise_error(ActiveRecord::RecordInvalid)
+      end
     end
 
     context "without a team" do
-      let(:player) { build(:player, team: nil) }
+      let(:team) { create(:team) }
+      let(:player) { create(:player, team: nil) }
 
-      it { expect(player).not_to be_valid }
+      it 'throws an error' do
+        expect { player }.to raise_error(ActiveRecord::RecordInvalid)
+      end
     end
   end
 
   describe "#full_name" do
     subject { player.full_name }
 
-    let(:player) { build(:player, first_name: first_name, last_name: last_name) }
+    let(:team) { create(:team) }
+    let(:player) { create(:player, team: team, first_name: first_name, last_name: last_name) }
     let(:first_name) { "Someone" }
     let(:last_name) { "Else" }
 
