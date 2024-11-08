dc5617787bcdcf62ac63d4407031e53955f5b068
Vinny VanSlyke <vincent.vanslyke@gmail.com>
2024-11-08 15:47:28 -0500

    finish saveable tests

--- spec/requests/modules/saveable_spec.rb
+++ spec/requests/modules/saveable_spec.rb
@@ -4,21 +4,47 @@ RSpec.describe Saveable do
   let(:saveable_class) { Class.new { include Saveable } }
   let(:saveable_instance) { saveable_class.new }
 
-  # def create_fielding_positions
-  #   positions = %w[LF LC RC RF P C 1B 2B SS 3B]
-  #   positions.each do |position|
-  #     rank = case position
-  #     when 'P', '1B' then 1
-  #     when '2B', 'SS', '3B' then 2
-  #     else 3
-  #     end
-  #     create(:fielding_position, name: position, hierarchy_rank: rank)
-  #   end
-  # end
+  def create_fielding_positions
+    positions = %w[LF LC RC RF P C 1B 2B SS 3B]
+    positions.each do |position|
+      rank = case position
+      when 'P', '1B' then 1
+      when '2B', 'SS', '3B' then 2
+      else 3
+      end
+      create(:fielding_position, name: position, hierarchy_rank: rank)
+    end
+  end
 
-  # before do
-  #   create_fielding_positions
-  # end
+  before do
+    create_fielding_positions
+  end
+
+  describe '#save_player_inning_assignments' do
+    let(:team) { create(:team) }
+    let(:number_of_innings) { 4 }
+    let(:game) { create(:game, inning_count: number_of_innings) }
+    let(:number_of_players) { 10 }
+    let(:players) { create_list(:player, number_of_players, team: team).sort }
+    let(:player_game_assignments) do
+      players.map do |player|
+        {
+          player_id: player.id,
+          game_assignments: [ '1B', '2B', 'LF', 'RF' ]
+        }
+      end
+    end
+    let(:product) { number_of_innings * number_of_players }
+    context 'when the player_game_assignments are completed and are valid' do
+      it 'creates \'player_innings\' for every player in each inning' do
+        expect(PlayerInning.all.count).to eq 0
+
+        saveable_instance.save_player_inning_assignments(player_game_assignments, game.id, 4)
+
+        expect(PlayerInning.all.count).to eq product
+      end
+    end
+  end
 
   describe '#save_player_counters' do
     let(:team) { create(:team) }
