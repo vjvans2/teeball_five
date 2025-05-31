9d827db2578ec14b698b0b0565b378a69dfaa9c3
turonn <72614966+turonn@users.noreply.github.com>
2025-05-31 13:10:37 -0400

    add game edit controlls

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,6 @@
 class GamesController < ApplicationController
+  before_action :_set_game, only: [ :show, :edit, :update, :destroy ]
+
   def new
     @game = Game.new
   end
@@ -6,49 +8,62 @@ class GamesController < ApplicationController
   def index
     # this is not sustainable for very long. Will eventually need to paginate
     # but this will work for now as we develop.
-    @games = Game.all
+    @games = Game.all.order(date: :desc, location: :asc)
   end
-  def show
-    @game = Game.find(params[:id])
-  end
+
+  def show; end
 
   def create
     @game = Game.new(_game_params)
     @game.season_id = 1
     if @game.save
-      gameday_team = create_gameday_team(@game.id)
+      gameday_team = _create_gameday_team(@game.id)
       redirect_to new_gameday_player_path(gameday_team_id: gameday_team[:id]), notice: "Game successfully created"
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-    # DELETE /games/:id
-    def destroy
-      @game = Game.find(params[:id])
-      @associated_gameday_team_id = GamedayTeam.find_by(game_id: params[:id]).id
+  def edit; end
 
-      PlayerInning.where(game_id: @game.id).delete_all
-      Inning.where(game_id: @game.id).delete_all
-      GamedayPlayer.where(gameday_team_id: @associated_gameday_team_id).delete_all
-      GamedayTeam.delete(@associated_gameday_team_id)
-
-      if @game.destroy
-        Rails.logger.info("Game #{@game.id} was successfully deleted")
-        redirect_to games_url, notice: "Game was successfully deleted."
-      else
-        Rails.logger.error("Failed to delete game #{@game.id}")
-        redirect_to games_url, alert: "Game could not be deleted."
-      end
+  # PATCH/PUT /games/:id
+  def update
+    if @game.update(_game_params)
+      redirect_to @game, notice: "Game was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
     end
+  end
+
+  # DELETE /games/:id
+  def destroy
+    @associated_gameday_team_id = GamedayTeam.find_by(game_id: params[:id]).id
+
+    PlayerInning.where(game_id: @game.id).delete_all
+    Inning.where(game_id: @game.id).delete_all
+    GamedayPlayer.where(gameday_team_id: @associated_gameday_team_id).delete_all
+    GamedayTeam.delete(@associated_gameday_team_id)
+
+    if @game.destroy
+      Rails.logger.info("Game #{@game.id} was successfully deleted")
+      redirect_to games_url, notice: "Game was successfully deleted."
+    else
+      Rails.logger.error("Failed to delete game #{@game.id}")
+      redirect_to games_url, alert: "Game could not be deleted."
+    end
+  end
 
   private
+
+  def _set_game
+    @game = Game.find(params[:id])
+  end
 
   def _game_params
     params.require(:game).permit(:location, :is_home, :opponent_name, :date)
   end
 
-  def create_gameday_team(game_id)
+  def _create_gameday_team(game_id)
     # needs to be created here instead of going to the GamedayTeamController because we cannot redirect directly to the CREATE action
     GamedayTeam.create!(
       game_id: game_id,
